94e796274b037cfc1ca5932baa73f2ba376bd57e
Gigliarly <gigliarlymarcelino@gmail.com>
2022-02-16 16:50:29 -0300

    Sem interações

---
+++ app/src/main/java/ifpb/mobile/login_interfase_mobile/FinalActivity.java
@@ -0,0 +1,14 @@
+package ifpb.mobile.login_interfase_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+    }
+}
