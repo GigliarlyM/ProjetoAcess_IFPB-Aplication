d6f77603a5176b5a69a5b13142a714eeb34b1787
fabricio <95913277+Araujp@users.noreply.github.com>
2022-02-18 18:35:46 -0300

    - Alteração nas telas: 1. activity_inicial.xml (tela 1); 2. activity_prots.xml (tela com a lista de protótipos, renomeada); 3. activity_estado.xml (tela com o estado do protótipo selecionado pelo usuário, criada);
    Pendências:
    
    - informações de logins: web-service entre o app e o servidor para retorno em formato JSON;
    - atribuir intents a tela com o estado dos protótipos (activity_estado.xml).
    
    PS: as partes do código em strings.xml e activity_estado.xml que estão comentadas, são importantes, mas estavam apresentando erros por estarem imcompletas (devido algumas partes que ainda estou pensando em como fazer);

--- app/src/main/java/ifpb/mobile/login_interfase_mobile/FinalActivity.java
+++ app/src/main/java/ifpb/mobile/login_interfase_mobile/FinalActivity.java
@@ -9,6 +9,6 @@ public class FinalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_final);
+        setContentView(R.layout.activity_prots);
     }
 }
