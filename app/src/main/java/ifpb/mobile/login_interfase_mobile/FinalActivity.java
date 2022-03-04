2fd4e3cb9590ba8cecad2dc3e358e98ba747a0e5
Gigliarly <gigliarlymarcelino@gmail.com>
2022-03-04 19:49:46 -0300

    Nesse commit foi adicionado um arquivo de gerenciamento de Dados;
    Adicionado interações com o usuario;
    E removeu os erros contidos.

--- app/src/main/java/ifpb/mobile/login_interfase_mobile/FinalActivity.java
+++ app/src/main/java/ifpb/mobile/login_interfase_mobile/FinalActivity.java
@@ -2,13 +2,33 @@ package ifpb.mobile.login_interfase_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class FinalActivity extends AppCompatActivity {
+public class FinalActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private RadioButton radio1;
+    private RadioButton radio2;
+    private RadioButton radio3;
+    private RadioButton radio4;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prots);
+
+        radio1 = findViewById(R.id.radioButtonPROT1);
+
+        intent = new Intent(this, InicialActivity.class);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/ifpb/mobile/login_interfase_mobile/seguranca/LoginUsuario.java
@@ -0,0 +1,14 @@
+package ifpb.mobile.login_interfase_mobile.seguranca;
+
+public class LoginUsuario {
+    public String usuario;
+    public String senha;
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
